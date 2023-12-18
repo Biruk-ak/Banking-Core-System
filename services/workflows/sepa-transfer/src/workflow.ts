4043a6b685f492259d32625fde8eca12b70675f6
Biruk-ak <birukaklilu0110@gmail.com>
2023-12-18 10:55:00 +0300

    Expand payment workflows: wire, SEPA, SWIFT

---
+++ services/workflows/sepa-transfer/src/workflow.ts
@@ -0,0 +1,150 @@
+/** SepaTransfer workflow — Banking Core System */
+export type SepaTransferStage =
+  | 'initiated'
+  | 'validated'
+  | 'compliance_check'
+  | 'risk_assessment'
+  | 'approval_pending'
+  | 'approved'
+  | 'executing'
+  | 'settled'
+  | 'rejected'
+  | 'cancelled';
+
+export interface SepaTransferContext {
+  workflowId: string;
+  customerId: string;
+  stage: SepaTransferStage;
+  amountMinor?: number;
+  currency?: string;
+  assignee?: string;
+  notes: string[];
+  checklist: Record<string, boolean>;
+  createdAt: string;
+  updatedAt: string;
+  metadata: Record<string, unknown>;
+}
+
+const TRANSITIONS: Record<SepaTransferStage, SepaTransferStage[]> = {
+  initiated: ['validated', 'cancelled'],
+  validated: ['compliance_check', 'rejected', 'cancelled'],
+  compliance_check: ['risk_assessment', 'rejected'],
+  risk_assessment: ['approval_pending', 'rejected'],
+  approval_pending: ['approved', 'rejected'],
+  approved: ['executing', 'cancelled'],
+  executing: ['settled', 'rejected'],
+  settled: [],
+  rejected: [],
+  cancelled: [],
+};
+
+export class SepaTransferWorkflow {
+  private ctx: SepaTransferContext;
+
+  constructor(customerId: string, metadata: Record<string, unknown> = {}) {
+    const now = new Date().toISOString();
+    this.ctx = {
+      workflowId: crypto.randomUUID(),
+      customerId,
+      stage: 'initiated',
+      notes: [],
+      checklist: {
+        identityVerified: false,
+        documentsComplete: false,
+        sanctionsCleared: false,
+        riskAccepted: false,
+        fundingConfirmed: false,
+      },
+      createdAt: now,
+      updatedAt: now,
+      metadata,
+    };
+  }
+
+  get context(): SepaTransferContext { return { ...this.ctx, notes: [...this.ctx.notes], checklist: { ...this.ctx.checklist } }; }
+
+  setAmount(amountMinor: number, currency: string): void {
+    this.ctx.amountMinor = amountMinor;
+    this.ctx.currency = currency;
+    this.touch();
+  }
+
+  check(item: keyof SepaTransferContext['checklist'], value = true): void {
+    this.ctx.checklist[item] = value;
+    this.touch();
+  }
+
+  note(text: string): void {
+    this.ctx.notes.push(`[${new Date().toISOString()}] ${text}`);
+    this.touch();
+  }
+
+  assign(userId: string): void {
+    this.ctx.assignee = userId;
+    this.touch();
+  }
+
+  transition(to: SepaTransferStage): void {
+    const allowed = TRANSITIONS[this.ctx.stage];
+    if (!allowed.includes(to)) {
+      throw new Error(`Invalid sepa-transfer transition ${this.ctx.stage} -> ${to}`);
+    }
+    if (to === 'compliance_check' && !this.ctx.checklist.identityVerified) {
+      throw new Error('Identity must be verified before compliance');
+    }
+    if (to === 'approved' && !this.ctx.checklist.riskAccepted) {
+      throw new Error('Risk must be accepted before approval');
+    }
+    this.ctx.stage = to;
+    this.note(`Transitioned to ${to}`);
+    this.touch();
+  }
+
+  isTerminal(): boolean {
+    return ['settled', 'rejected', 'cancelled'].includes(this.ctx.stage);
+  }
+
+  completionRatio(): number {
+    const values = Object.values(this.ctx.checklist);
+    return values.filter(Boolean).length / values.length;
+  }
+
+  private touch(): void {
+    this.ctx.updatedAt = new Date().toISOString();
+  }
+}
+
+export class SepaTransferWorkflowService {
+  private store = new Map<string, SepaTransferWorkflow>();
+
+  start(customerId: string, metadata?: Record<string, unknown>): SepaTransferWorkflow {
+    const wf = new SepaTransferWorkflow(customerId, metadata);
+    this.store.set(wf.context.workflowId, wf);
+    return wf;
+  }
+
+  get(id: string): SepaTransferWorkflow | undefined {
+    return this.store.get(id);
+  }
+
+  listByCustomer(customerId: string): SepaTransferContext[] {
+    return [...this.store.values()].map((w) => w.context).filter((c) => c.customerId === customerId);
+  }
+
+  advanceToSettlement(id: string): SepaTransferContext {
+    const wf = this.store.get(id);
+    if (!wf) throw new Error('Workflow not found');
+    const path: SepaTransferStage[] = ['validated', 'compliance_check', 'risk_assessment', 'approval_pending', 'approved', 'executing', 'settled'];
+    wf.check('identityVerified');
+    wf.check('documentsComplete');
+    wf.check('sanctionsCleared');
+    wf.check('riskAccepted');
+    wf.check('fundingConfirmed');
+    for (const stage of path) {
+      if (!wf.isTerminal() && wf.context.stage !== stage) {
+        try { wf.transition(stage); } catch { /* skip if already past */ }
+      }
+    }
+    return wf.context;
+  }
+}
---
+++ services/workflows/swift-mt/src/workflow.ts
@@ -0,0 +1,150 @@
+/** SwiftMt workflow — Banking Core System */
+export type SwiftMtStage =
+  | 'initiated'
+  | 'validated'
+  | 'compliance_check'
+  | 'risk_assessment'
+  | 'approval_pending'
+  | 'approved'
+  | 'executing'
+  | 'settled'
+  | 'rejected'
+  | 'cancelled';
+
+export interface SwiftMtContext {
+  workflowId: string;
+  customerId: string;
+  stage: SwiftMtStage;
+  amountMinor?: number;
+  currency?: string;
+  assignee?: string;
+  notes: string[];
+  checklist: Record<string, boolean>;
+  createdAt: string;
+  updatedAt: string;
+  metadata: Record<string, unknown>;
+}
+
+const TRANSITIONS: Record<SwiftMtStage, SwiftMtStage[]> = {
+  initiated: ['validated', 'cancelled'],
+  validated: ['compliance_check', 'rejected', 'cancelled'],
+  compliance_check: ['risk_assessment', 'rejected'],
+  risk_assessment: ['approval_pending', 'rejected'],
+  approval_pending: ['approved', 'rejected'],
+  approved: ['executing', 'cancelled'],
+  executing: ['settled', 'rejected'],
+  settled: [],
+  rejected: [],
+  cancelled: [],
+};
+
+export class SwiftMtWorkflow {
+  private ctx: SwiftMtContext;
+
+  constructor(customerId: string, metadata: Record<string, unknown> = {}) {
+    const now = new Date().toISOString();
+    this.ctx = {
+      workflowId: crypto.randomUUID(),
+      customerId,
+      stage: 'initiated',
+      notes: [],
+      checklist: {
+        identityVerified: false,
+        documentsComplete: false,
+        sanctionsCleared: false,
+        riskAccepted: false,
+        fundingConfirmed: false,
+      },
+      createdAt: now,
+      updatedAt: now,
+      metadata,
+    };
+  }
+
+  get context(): SwiftMtContext { return { ...this.ctx, notes: [...this.ctx.notes], checklist: { ...this.ctx.checklist } }; }
+
+  setAmount(amountMinor: number, currency: string): void {
+    this.ctx.amountMinor = amountMinor;
+    this.ctx.currency = currency;
+    this.touch();
+  }
+
+  check(item: keyof SwiftMtContext['checklist'], value = true): void {
+    this.ctx.checklist[item] = value;
+    this.touch();
+  }
+
+  note(text: string): void {
+    this.ctx.notes.push(`[${new Date().toISOString()}] ${text}`);
+    this.touch();
+  }
+
+  assign(userId: string): void {
+    this.ctx.assignee = userId;
+    this.touch();
+  }
+
+  transition(to: SwiftMtStage): void {
+    const allowed = TRANSITIONS[this.ctx.stage];
+    if (!allowed.includes(to)) {
+      throw new Error(`Invalid swift-mt transition ${this.ctx.stage} -> ${to}`);
+    }
+    if (to === 'compliance_check' && !this.ctx.checklist.identityVerified) {
+      throw new Error('Identity must be verified before compliance');
+    }
+    if (to === 'approved' && !this.ctx.checklist.riskAccepted) {
+      throw new Error('Risk must be accepted before approval');
+    }
+    this.ctx.stage = to;
+    this.note(`Transitioned to ${to}`);
+    this.touch();
+  }
+
+  isTerminal(): boolean {
+    return ['settled', 'rejected', 'cancelled'].includes(this.ctx.stage);
+  }
+
+  completionRatio(): number {
+    const values = Object.values(this.ctx.checklist);
+    return values.filter(Boolean).length / values.length;
+  }
+
+  private touch(): void {
+    this.ctx.updatedAt = new Date().toISOString();
+  }
+}
+
+export class SwiftMtWorkflowService {
+  private store = new Map<string, SwiftMtWorkflow>();
+
+  start(customerId: string, metadata?: Record<string, unknown>): SwiftMtWorkflow {
+    const wf = new SwiftMtWorkflow(customerId, metadata);
+    this.store.set(wf.context.workflowId, wf);
+    return wf;
+  }
+
+  get(id: string): SwiftMtWorkflow | undefined {
+    return this.store.get(id);
+  }
+
+  listByCustomer(customerId: string): SwiftMtContext[] {
+    return [...this.store.values()].map((w) => w.context).filter((c) => c.customerId === customerId);
+  }
+
+  advanceToSettlement(id: string): SwiftMtContext {
+    const wf = this.store.get(id);
+    if (!wf) throw new Error('Workflow not found');
+    const path: SwiftMtStage[] = ['validated', 'compliance_check', 'risk_assessment', 'approval_pending', 'approved', 'executing', 'settled'];
+    wf.check('identityVerified');
+    wf.check('documentsComplete');
+    wf.check('sanctionsCleared');
+    wf.check('riskAccepted');
+    wf.check('fundingConfirmed');
+    for (const stage of path) {
+      if (!wf.isTerminal() && wf.context.stage !== stage) {
+        try { wf.transition(stage); } catch { /* skip if already past */ }
+      }
+    }
+    return wf.context;
+  }
+}
